f304d6e8d8c51aae85d2645641348856b71f35b5
Stefan Staub <sstaub@users.noreply.github.com>
2020-09-29 12:19:50 +0200

    Update TeensyID.h

--- TeensyID.h
+++ TeensyID.h
@@ -22,44 +22,6 @@
  * SOFTWARE.
  */
 
-/** Teensy Library for different IDs of Teensy LC and Teensy 3.0 ... 3.6
-  *
-  * - USB Serial Number
-  * - Teensy Serial Number
-  * - MAC Address
-  * - Kinetis Chip UID
-  * - UUID (RFC4122) pseudo v4
-  *
-  * @code
-  *  #include <TeensyID.h>
-  *
-  * uint8_t serial[4];
-  * uint8_t mac[6];
-  * uint32_t uid[4];
-  * uint8_t uuid[16];
-  *
-  * void setup() {
-  *   teensySN(serial);
-  *   teensyMAC(mac);
-  *   kinetisUID(uid);
-  *   teensyUUID(uuid);
-  *   delay(2000);
-  *   Serial.printf("USB Serialnumber: %u \n", teensyUsbSN());
-  *   Serial.printf("Array Serialnumber: %02X-%02X-%02X-%02X \n", serial[0], serial[1], serial[2], serial[3]);
-  *   Serial.printf("String Serialnumber: %s\n", teensySN());
-  *   Serial.printf("Array MAC Address: %02X:%02X:%02X:%02X:%02X:%02X \n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  *   Serial.printf("String MAC Address: %s\n", teensyMAC());
-  *   Serial.printf("Array 128-bit UniqueID from chip: %08X-%08X-%08X-%08X\n", uid[0], uid[1], uid[2], uid[3]);
-  *   Serial.printf("String 128-bit UniqueID from chip: %s\n", kinetisUID());
-  *   Serial.printf("Array 128-bit UUID RFC4122: %02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X\n", uuid[0], uuid[1], uuid[2], uuid[3], uuid[4], uuid[5], uuid[6], uuid[7], uuid[8], uuid[9], uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]);
-  *   Serial.printf("String 128-bit UUID RFC4122: %s\n", teensyUUID());
-  *   }
-  *
-  * void loop() {}
-  *
-  * @endcode
-  */
-
 #ifndef TEENSYID_H
 #define TEENSYID_H
 
